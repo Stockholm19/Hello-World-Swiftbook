e489fad069e0bdcc0d9f528235894b50690d52e9
Abrikozs <Abrikozs@gmail.com>
2024-10-14 15:44:19 +0300

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Роман Пшеничников on 14.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hello WorldTests/Hello_WorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldTests.swift
+//  Hello WorldTests
+//
+//  Created by Роман Пшеничников on 14.10.2024.
+//
+
+import Testing
+@testable import Hello_World
+
+struct Hello_WorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
